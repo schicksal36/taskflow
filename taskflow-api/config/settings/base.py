36d189d05040e73772423c4c812faa0cfe9488a5
“schicksal36” <schicksal36@naver.com>
2026-06-30 23:53:46 +0900

    update project changes

--- taskflow-api/config/settings/base.py
+++ taskflow-api/config/settings/base.py
@@ -152,7 +152,7 @@ SITE_ID = env.int("SITE_ID", default=1)
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-MEDIA_URL = "/media/"
+MEDIA_URL = env("MEDIA_URL", default="/media/")
 MEDIA_ROOT = BASE_DIR / "media"
 
 FRONTEND_URL = env("FRONTEND_URL", default="http://localhost:3000")
